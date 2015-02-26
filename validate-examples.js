88d9e312152e3201a4a9a3240cf6bfbc7c0ac997
Hajo Eichler <hajo.eichler@commercetools.de>
2015-02-26 21:20:13 +0100

    Refactor loading of types.

--- validate-examples.js
+++ validate-examples.js
@@ -3,32 +3,37 @@ var Validator = require('jsonschema').Validator;
 var val = new Validator();
 var currentDir = __dirname;
 
-var ltextFileName = currentDir + '/schema/types/ltext.json';
-var ltext = require('' + ltextFileName);
-val.addSchema(ltext)
+// add types to validator
+var typeNames = fs.readdirSync(currentDir + '/schema/types');
+for (var i = 0; i < typeNames.length; i++) {
+  var typeFileName = currentDir + '/schema/types/' + typeNames[i];
+  console.log('Reading type: ' + typeFileName);
+  var type = require('' + typeFileName);
+  val.addSchema(type)
+}
 
 var referenceFileName = currentDir + '/schema/types/reference.json';
 var reference = require('' + referenceFileName);
 val.addSchema(reference)
 
-var types = fs.readdirSync(currentDir + '/examples');
+var schemaNames = fs.readdirSync(currentDir + '/examples');
 
-// TODO: add verbose / quite options
-for (var i = 0; i < types.length; i++) {
-  type = types[i];
+// TODO: add verbose / quiet options
+for (var i = 0; i < schemaNames.length; i++) {
+  schemaName = schemaNames[i];
   // read schema definition
-  var schemaFileName = currentDir + '/schema/' + type + '.schema.json';
+  var schemaFileName = currentDir + '/schema/' + schemaName + '.schema.json';
   console.log('Reading schema: ' + schemaFileName);
   var schema = require('' + schemaFileName);
 
   // read examples related to current schema
-  var targetDir = currentDir + '/examples/' + type;
+  var targetDir = currentDir + '/examples/' + schemaName;
   var examples = fs.readdirSync(targetDir);
   if (!examples) {
     console.warn('No files found in directory: ' + targetDir);
   }
   for (var j = 0; j < examples.length; j++) {
-    var exampleFileName = currentDir + '/examples/' + type + '/' + examples[j];
+    var exampleFileName = currentDir + '/examples/' + schemaName + '/' + examples[j];
     console.log('Reading example: ' + exampleFileName);
     var example = require('' + exampleFileName);
 
